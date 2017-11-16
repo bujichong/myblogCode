3d305f4be0ca904d7e9c1a5a29c7d83b12e188fe
bujichong <bujichong@163.com>
2017-11-16 17:37:29 +0800

    相册功能基本完成

--- source/photo/gallery.js
+++ source/photo/gallery.js
@@ -1,7 +1,9 @@
 "use strict";
+
 const fs = require("fs");
 const images = require("images");
 const path = "../../photos/";
+const outfile = "gallery.json";
 const sourcePath = path + "source";
 const galleryPath = path + "gallery";
 const thumbPath = path + "thumb";
@@ -17,25 +19,29 @@ fs.readdir(sourcePath, function (err, files) {
         const img = images(sourcePath + "/" + name)//加载图像文件
         const size = img.size();
         const wh = size.width/size.height;
-        let newsize;
+        let newsize = size;
 
-        if(wh>1){
-            img.resize(maxsize)//等比缩放图像到400像素宽
-            newsize = {width:maxsize,height:Math.floor(maxsize/wh)};
-        }else{
-            img.resize(null,maxsize)//等比缩放图像到400像素高
-            newsize = {width:Math.floor(maxsize*wh),height:maxsize};
+        if(wh>=1){//图片宽>高
+            if (maxsize<size.width) {
+                img.resize(maxsize)//等比缩放图像到maxsize像素宽
+                newsize = {width:maxsize,height:Math.floor(maxsize/wh)};
+            };
+        }else{//图片宽<高
+            if(maxsize<size.height){
+                img.resize(null,maxsize)//等比缩放图像到maxsize像素高
+                newsize = {width:Math.floor(maxsize*wh),height:maxsize};
+            }
         }
         img.save(path + "/" + name, {
             quality : quality  //保存图片到文件,图片质量为60
         });
-        return newsize;
+        return {newsize};
     }
 
     (function iterator(index) {
 
         if (index == files.length) {
-            fs.writeFile("gallery.json", JSON.stringify(arr, null, "\t"));
+            fs.writeFile(outfile, JSON.stringify(arr, null, "\t"));
             return;
         }
 
@@ -44,21 +50,22 @@ fs.readdir(sourcePath, function (err, files) {
                 return;
             }
 
-            resizeImg(files[index],thumbPath,400,90);
-            let newsize = resizeImg(files[index],galleryPath,900,80);
+            resizeImg(files[index],thumbPath,300,90);
+            let sz = resizeImg(files[index],galleryPath,1500,80);
 
             if (stats.isFile()) {
                 arr.push({
                     name:files[index],
-                    width:newsize.width,
-                    height:newsize.height,
+                    // ow:sz.size.width,
+                    // oh:sz.size.height,
+                    w:sz.newsize.width,
+                    h:sz.newsize.height,
                     time:stats.mtime,
                     timeString:stats.mtimeMs
                 });
             }
             iterator(index + 1);
         })
-
 
     }(0));
 });
